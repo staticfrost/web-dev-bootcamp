87c99fc7d475471222146639d40f7cba07b72393
staticfrost <scott@scottbailey.net>
2018-03-18 15:02:25 +1000

    learning the real basics.

---
+++ intro javascript/firstex.js
@@ -0,0 +1,10 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+//or another options
+//var fullName = firstName + " " + lastName
+// console.log("Your full name is " + fullName);
+
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("You are " + age + " years old");
